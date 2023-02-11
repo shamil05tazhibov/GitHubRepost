ad3d359b50d34ca9f0c0c4d2871fe14b75479bf8
Shamil Tazhibov <tazhibov90@mail.ru>
2023-02-11 10:29:02 +0300

    Внес изменения в AppCompatActivity

--- app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
+++ app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("MainActivity","Был вызван onCreate")
-        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Был вызван onCreate")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("MainActivity","Был вызван onDestroy")
+        Log.d("MainActivity", "Был вызван onDestroy")
     }
 }
