0cd11dfb8f89dd78875ec155bfd5aebc38f1ac54
Shamil Tazhibov <tazhibov90@mail.ru>
2023-02-11 10:04:30 +0300

    Добавил логи для onCreate в MainActivity

--- app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
+++ app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.tazhibov.githubrepos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity","onCreate")
         setContentView(R.layout.activity_main)
     }
 }
