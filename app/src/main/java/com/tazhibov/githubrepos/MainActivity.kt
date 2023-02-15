458c50b99623e0a0c5cc69651dc9414b6e89c754
Shamil Tazhibov <tazhibov90@mail.ru>
2023-02-15 09:51:33 +0300

    Добавил коментарии поясняющие для onCreate и onDestroy

--- app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
+++ app/src/main/java/com/tazhibov/githubrepos/MainActivity.kt
@@ -5,12 +5,13 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
+    //    Вызывается при создании Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "Был вызван onCreate")
     }
 
+    // Вызывается при уничтожении экрана
     override fun onDestroy() {
         super.onDestroy()
         Log.d("MainActivity", "Был вызван onDestroy")
@@ -19,12 +20,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     //Вызывается когда экран становиться видимым
     override fun onStart() {
         super.onStart()
-        Log.d("MainActivity","Был вызван onStart")
+        Log.d("MainActivity", "Был вызван onStart")
     }
 
     //Вызывается когда экран становиться не видемым
     override fun onStop() {
         super.onStop()
-        Log.d("MainActivity","был вызван onStop")
+        Log.d("MainActivity", "был вызван onStop")
     }
 }
